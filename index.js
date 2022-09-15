e3e692048aa6eb6564613af24370440e9e035229
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-15 00:34:06 -0300

    se añadieron dependencias y nodemon

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const password = 'purpl3sun'
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 3400;
+const password = 'purpl3sun';
+const URL = `mongodb+srv://walter-daniel:${password}@cluster0.g08hg.mongodb.net/?retryWrites=true&w=majority`;
 
-const URL = `mongodb+srv://walter-daniel:${password}@cluster0.g08hg.mongodb.net/?retryWrites=true&w=majority`
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+
+(async function connectDB() {
+    try {
+        console.log('\x1b[36m iniciando la conexión\x1b[37m')
+        await mongoose.connect(URL);
+        console.log('\x1b[32m conectado a la base de datos \x1b[37m')
+        app.listen(port, () => {
+            console.log(`\x1b[35m Escuchando desde el puerto: ${port} \x1b[37m`)
+        });
+    } catch (error) {
+        console.log('\x1b[31m error al conectarse a la base de datos \x1b[37m', error)
+    }    
+})();
