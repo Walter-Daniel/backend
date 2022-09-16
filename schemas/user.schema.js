dbf1820eb4b92ae0bfe67251c7852562c2832cf3
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-16 14:51:42 -0300

    modificación schema del usuario

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,13 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const VALID_ROLES = [
+    'ADMIN_ROLE',
+    'USER_ROLE',
+];
+
 const UserSchema = new Schema({
-    fullName: { type: String, require: true },
-    email: { type: String, require: true },
-    password: { type: String, require: true },
-    age: Number,
-    active: Boolean,
-    role: String
+    fullName: { 
+        type: String, 
+        require: true, 
+        minlength: 5, 
+        maxlength: 40 
+    },
+    email: { 
+        type: String, 
+        require: true,
+        unique: true,
+        uniqueCaseInsensitive: true,
+        index: true, 
+    },
+    password: { 
+        type: String, 
+        require: true
+    },
+    active: {
+        type: Boolean,
+        require: true,
+        default: false
+    },
+    role: {
+        type: String,
+        enum: VALID_ROLES,
+        default: VALID_ROLES[1]
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema)
