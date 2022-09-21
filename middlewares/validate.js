a0a19b0f88d5904b818f57c3d0c7f6ba1c549476
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-21 20:34:37 -0300

    Se modificó validate en middleware y en rutas

---
+++ middlewares/validate.js
@@ -0,0 +1,24 @@
+const { validationResult } = require('express-validator');
+
+const validate = (req, res, next) => {
+    console.log('segundo')
+    const errors = validationResult(req);
+
+    if(errors.isEmpty()) {
+        next()
+    } else {
+        const extractedErrors = errors.array().map(err => {
+            console.log(err);
+            return {
+                [err.param]: err.msg
+            }
+        })
+        return res.status(422).send({
+            ok: false,
+            errors: extractedErrors
+        })
+        }
+    }
+   
+
+module.exports = validate
