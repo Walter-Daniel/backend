dfb5d82f3aeec52deba264b4b31febfbd6223cd8
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-09-29 21:17:07 -0300

    Auth controller, routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,6 +3,5 @@ const api = express.Router();
 const orderController = require('../controllers/order.controller')
 
 api.get('/categories/:name', orderController.getOrders)
-api.post('/orders', orderController.createOrder)
 
 module.exports = api
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,19 +3,13 @@ const api = express.Router();
 const userController = require('../controllers/user.controller');
 const jwtVerify = require('../middlewares/jwt');
 const isAdmin = require('../middlewares/isAdmin');
-const userLoginValidator = require('../middlewares/userValidator');
-const validate = require('../middlewares/validate')
 
 api.get('/user/:userID', jwtVerify, userController.getUser);
 
 api.get('/users/:name?', jwtVerify, userController.getUsers);
 
-api.post('/users', userController.createUser);
-
 api.put('/users', [jwtVerify], userController.editUser);
 
 api.delete('/users/:id', [jwtVerify, isAdmin], userController.deleteUser);
-
-api.post('/login', userLoginValidator(), validate , userController.login);
 
 module.exports = api
