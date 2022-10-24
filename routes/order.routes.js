598dae96b68c9530153c5105ce1f55a43b48e87c
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-10-23 23:18:21 -0300

    solución al error de rutas en orders

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const api = express.Router();
-const orderController = require('../controllers/order.controller')
+const orderController = require('../controllers/order.controller');
+const jwtVerify = require('../middlewares/jwt');
+const isAdmin = require('../middlewares/isAdmin');
 
 api.get('/orders', [jwtVerify, isAdmin], orderController.getOrders)
 api.post('/orders', [jwtVerify, isAdmin], orderController.createOrder)
