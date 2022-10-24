9e508ba087cf7ff4e5cfca58a379a34c9ac06a12
Walter Daniel <walterdcarrizo.19@gmail.com>
2022-10-23 21:10:56 -0300

    solución al editar una orden

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const api = express.Router();
 const orderController = require('../controllers/order.controller')
 
-api.get('/orders', orderController.getOrders)
-api.post('/orders', orderController.createOrder)
+api.get('/orders', [jwtVerify, isAdmin], orderController.getOrders)
+api.post('/orders', [jwtVerify, isAdmin], orderController.createOrder)
+api.put('/orders', [jwtVerify, isAdmin], orderController.updateOrders)
 
 module.exports = api
